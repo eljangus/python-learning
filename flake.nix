5411400e9f2f16294f584e60e97831e3a1869d6a
Elias Schlosser <eljangus@gmail.com>
2026-07-27 15:52:13 +0200

    pimped up devshell using flake-utils

--- flake.nix
+++ flake.nix
@@ -2,15 +2,16 @@
   description = "devshell for learning python";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { nixpkgs, self }: 
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      devShells.${system}.default = pkgs.callPackage ./nix/devshell.nix {
-      }; 
-    };
+  outputs = { nixpkgs, flake-utils, ... }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.callPackage ./nix/devshell.nix {
+        }; 
+      });
 }
