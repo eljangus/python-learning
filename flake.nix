45de68e06dea4d8f273e5416844e7511bc71bf60
Elias Schlosser <eljangus@gmail.com>
2026-07-28 00:00:59 +0200

    improved flake

--- flake.nix
+++ flake.nix
@@ -4,29 +4,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
   outputs = { nixpkgs, ... }:
-      let
-        inherit (nixpkgs.lib) genAttrs;
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ];
-        forEachSystem =
-          perSystem:
-          genAttrs systems (
-            system:
-            let
-              pkgs = nixpkgs.legacyPackages.${system};
-            in
-              perSystem { inherit pkgs system; }
-          );
-      in {
-        packages =  forEachSystem (
-          { pkgs, ...}:
-            {
-              default = pkgs.callPackage ./nix/devshell.nix {};
-            }
-          );
-        };
+    let
+      inherit (nixpkgs.lib) genAttrs;
+      inherit (nixpkgs.lib.systems) flakeExposed;
+      forEachSystem =
+        perSystem:
+        genAttrs flakeExposed (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          perSystem { inherit pkgs system; }
+        );
+    in
+    {
+      devShells = forEachSystem (
+        { pkgs, ... }:
+        {
+          default = pkgs.callPackage ./nix/devshell.nix { };
+        }
+      );
+    };
 }
