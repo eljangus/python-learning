4ccf3b6d3144b3513e260c3b624f55c7970fe89d
Elias Schlosser <eljangus@gmail.com>
2026-07-27 23:40:50 +0200

    improved flake to remove flake utils

--- flake.nix
+++ flake.nix
@@ -2,16 +2,31 @@
   description = "devshell for learning python";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { nixpkgs, flake-utils, ... }: 
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, ... }:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        inherit (nixpkgs.lib) genAttrs;
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
+        forEachSystem =
+          perSystem:
+          genAttrs systems (
+            system:
+            let
+              pkgs = nixpkgs.legacyPackages.${system};
+            in
+              perSystem { inherit pkgs system; }
+          );
       in {
-        devShells.default = pkgs.callPackage ./nix/devshell.nix {
-        }; 
-      });
+        packages =  forEachSystem (
+          { pkgs, ...}:
+            {
+              default = pkgs.callPackage ./nix/devshell.nix {};
+            }
+          );
+        };
 }
