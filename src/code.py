a98f8c5e9bd28b8eb95b6f33d094ce1edb8f82fc
Elias Schlosser <eljangus@gmail.com>
2026-08-03 20:52:44 +0200

    learned a bit

--- src/code.py
+++ src/code.py
@@ -1,5 +1,20 @@
-x = 3
-if x / 2 == 1.5:
-    print("Hello, World")
+firstnum = input("first number: ")
+operator = input("operator: ")
+secondnum = input("second num: ")
+firstnum = float(firstnum)
+secondnum = float(secondnum)
+
+print(firstnum, operator, secondnum)
+check = input("is this correct? (y/n) ")
+if check == "y":
+    match operator:
+        case "-":
+            print(firstnum-secondnum)
+        case "+":
+            print(firstnum+secondnum)
+        case "*":
+            print(firstnum*secondnum)
+        case "/":
+            print(firstnum/secondnum)
 else:
-    print("nuh")
+    print("go kys")
