78e2c03b09df78d6033308c75562ec6490149858
Elias Schlosser <eljangus@gmail.com>
2026-08-09 19:02:10 +0200

    doing some learning

---
+++ src/bmi.py
@@ -0,0 +1,11 @@
+def fatty(name, weight, height):
+    BMI = weight / (height*height)
+    if BMI >= 25:
+        print(name, "du bist ein Fettsack! Dein BMI beträgt:", BMI)
+    elif BMI <= 25 and BMI >= 18.5:
+        print(name, "du bist gesund! Dein BMI beträgt:", BMI)
+    elif BMI <= 18.5:
+        print(name, "du bist ein Stock! Dein BMI beträgt:", BMI)
+
+
+fatty(input("Wie lautet dein Name?: "), float(input("Was ist dein Gewicht in kg?: ")), float(input("Was ist deine Größe in m?: ")))
