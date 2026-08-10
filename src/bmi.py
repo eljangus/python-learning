46da53d3355d6b12785aa9c106e21739470f8874
Elias Schlosser <eljangus@gmail.com>
2026-08-10 15:23:48 +0200

    practice exceptions and dictionaries

--- src/bmi.py
+++ src/bmi.py
@@ -1,12 +1,20 @@
-def fatty(name, weight, height):
-    BMI = weight / (height*height)
-    if BMI >= 25:
-        print(name, "du bist ein Fettsack! Dein BMI beträgt:", BMI)
-    elif BMI <= 25 and BMI >= 18.5:
-        print(name, "du bist gesund! Dein BMI beträgt:", BMI)
-    elif BMI <= 18.5:
-        print(name, "du bist ein Stock! Dein BMI beträgt:", BMI)
-    return BMI, name
+print("Welcome to the BMI calculator!")
+
+dictionary = dict()
 
 
-fatty(input("Wie lautet dein Name?: "), float(input("Was ist dein Gewicht in kg?: ")), float(input("Was ist deine Größe in m?: ")))
+while True:
+    name = str(input("What is your name?: "))
+    weight = float(input("What is your weight in kg?: "))
+    height = float(input("What is your height in m?: "))
+    check = str(input("is that everyone? (yes/no): "))
+    bmi = weight / (height**2)
+    if check == "yes":
+        dictionary[name] = bmi
+        for name, bmi in dictionary.items():
+            print(name, ":", round(bmi, 2))
+        break
+    elif check == "no":
+        dictionary[name] = bmi
+    else:
+        raise Exception("""input has to be either "yes" or "no"!""")
