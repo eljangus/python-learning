bb070b92848354c5090ee2e6e964df403168837c
Elias Schlosser <eljangus@gmail.com>
2026-08-24 21:26:19 +0200

    worked on my todo list!

--- src/pkgs/todo.py
+++ src/pkgs/todo.py
@@ -7,53 +7,96 @@ dirname = f"{Path.home()}/.config/todopy/"
 if os.path.exists(filename):
     pass
 else:
-    os.mkdir(dirname)
     with open(filename, "w") as f:
+        f.write("General TO-DO list!\n")
+
+if os.path.exists(dirname):
+    pass
+else:
+    os.mkdir(dirname)
+
+
+class Prompt:
+    def __init__(self):
         pass
+
+    def welcome(self):
+        print(f"1. List todo files")
+        print(f"2. Append to todo files")
+        print(f"3. Remove Lines from todo files")
+        print(f"4. Delete todo files")
+        self.number_of_options = 4
+        return self.number_of_options
+
+    def process_input(self):
+        while True:
+            match main_input:
+                case 1:
+                    todo.read_dir()
+                    break
+                case 2:
+                    todo.get_file_path()
+                    n = 1
+                    for i in todo.names_of_filepaths:
+                        print(f"{n}. {i}")
+                        n += 1
+                    print("")
+                    sub_input = int(input("Which file do you wish to append to?:\n"))
+                    print("")
+                    todo_file = todo.list_of_filepaths[sub_input - 1]
+                    todo.append_file(todo_file)
+                    break
+                case 3:
+                    todo.read_dir()
+                    break
+                case 4:
+                    todo.read_dir()
+                    break
 
 
 class Todo:
     def __init__(self):
         pass
 
-    def read_file(self, todo_file):
+    def read_file(self, todo_file: str):
         with open(todo_file, "r") as f:
             self.read = f.read()
         return self.read
+
+    def get_file_path(self):
+        self.list_of_filepaths = list()
+        self.names_of_filepaths = list()
+        for e in os.scandir(dirname):
+            if e.is_file():
+                self.list_of_filepaths.append(e.path)
+                self.names_of_filepaths.append(e.name)
+        return self.list_of_filepaths, self.names_of_filepaths
 
     def read_dir(self):
         for e in os.scandir(dirname):
             if e.is_file():
                 with open(e.path, "r") as f:
-                    print(f"\nHeader of {e.name}:")
-                    print(f"{f.readline().rstrip("\n")}")
+                    print(f"{e.name}:")
+                    print(f"{f.readline().rstrip('\n')}")
+                    print("")
 
-    def append_file(self, todo_file):
+    def append_file(self, todo_file: str):
         with open(todo_file, "a") as f:
-            self.append = f.append(
-                input(f"What do you wish to append to your TO-DO entry?:\n")
+            self.append = f.write(
+                input(f"What do you wish to append to your TO-DO entry?:\n") + "\n"
             )
         return self.append
 
 
-obj = Todo()
-while True:
-    print(f"1. List todo files")
-    print(f"2. Append to todo files")
-    print(f"3. Remove Lines from todo files")
+if __name__ == "__main__":
+    todo = Todo()
+    welcome = Prompt()
+    welcome.welcome()
     try:
-        input = int(input(f"\nSelect an option:\n"))
+        main_input = int(input(f"\nSelect an option:\n"))
+        print("")
     except ValueError:
         print("Input must be an integer!")
-    if input not in [1, 2, 3]:
-        raise Exception("Input must range from 1 to 3")
-    match input:
-        case 1:
-            obj.read_dir()
-            break
-        case 2:
-            obj.read_dir()
-            break
-        case 3:
-            obj.read_dir()
-            break
+    if main_input not in [i for i in range(1, welcome.number_of_options + 1)]:
+        raise Exception(f"Input must range from 1 to {welcome.number_of_options}")
+    welcome.process_input()
