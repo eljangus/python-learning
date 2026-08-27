c94536035cdb3df9e96733bf10a9391117487c47
Elias Schlosser <eljangus@gmail.com>
2026-08-27 02:43:32 +0200

    fixed semantics for main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from pkgs import todo as td
 
-if __name__ == "__main__":
-    td.welcome.initial_check()
-    td.welcome.welcome()
-    td.welcome.process_input()
+td.welcome.initial_check()
+td.welcome.welcome()
+td.welcome.process_input()
